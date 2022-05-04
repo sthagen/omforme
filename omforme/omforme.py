a26a49740b07c35cdb71053a1ab8735fbc847b44
Stefan Hagen <stefan@hagen.link>
2022-05-04 22:06:45 +0200

    Added passing doctest to class

--- omforme/omforme.py
+++ omforme/omforme.py
@@ -33,6 +33,25 @@ class Omforme:
 
     Actions are epected to be within ('ignore', 'collect', 'return')
 
+    Example:
+
+    >>> # from omforme.omforme import Omforme
+    >>> gen = (x for x in ('a', 'b', 'c', 'c', 'c', 'd', '1', '2', '3', 'e'))
+    >>> playbook = (('b', 'ignore', lambda: None), ('d', 'collect', list()), ('e', 'return', lambda: None),)
+    >>> transform = Omforme(playbook)(gen) # doctest: +ELLIPSIS
+    0 3 a b ignore <function <lambda> at 0x...> d
+    0 3 b b ignore <function <lambda> at 0x...> d
+    0 3 c b ignore <function <lambda> at 0x...> d
+    0 3 c b ignore <function <lambda> at 0x...> d
+    0 3 c b ignore <function <lambda> at 0x...> d
+    1 3 1 d collect ['1'] e
+    1 3 2 d collect ['1', '2'] e
+    1 3 3 d collect ['1', '2', '3'] e
+    1 3 e d collect ['1', '2', '3'] e
+    >>> transform[1]
+    ('d', 'collect', ['1', '2', '3'])
+    >>>
+
     """
     def __init__(self, playbook):
         """Later alligator."""
